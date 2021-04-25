a30bc3f89162379dc7fbde9ba72b8ea23bd11298
Enmanuel Ramirez <enmanuelr7@gmail.com>
2021-04-24 23:01:12 -0300

    icons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Notes from './pages/Notes';
+import Create from './pages/Create';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <Container>
+      <Router>
+        <CssBaseline />
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        component="h2"
+        color="textSecondary"
+        gutterBottom
+      >
+        Create A New Note
+      </Typography>
+      <Button type="submit" color="secondary" variant="contained" endIcon={<NavigateNextIcon />}>
+        Submit
+      </Button>
     </div>
-  )
+  );
 }
